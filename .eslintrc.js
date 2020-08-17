4c60e897dcee5e34747f739702a58be2cc2e99bf
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-08-17 15:44:18 +0200

    update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'warn',
     'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
 };
