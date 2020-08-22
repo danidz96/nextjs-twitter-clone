fe73306349d0e3f9e757cac4d14b9aecad8b4100
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-08-22 16:43:52 +0200

    create HomePage

---
+++ pages/home/styles.js
@@ -0,0 +1,26 @@
+import css from 'styled-jsx/css';
+
+export default css`
+  header {
+    height: 4.9rem;
+    position: fixed;
+    border-bottom: 1px solid #ccc;
+    top: 0;
+    width: 100%;
+  }
+
+  section {
+    padding-top: 4.9rem;
+  }
+
+  h2 {
+    font-weight: 800;
+    font-size: 2.2rem;
+  }
+
+  nav {
+    position: fixed;
+    bottom: 0;
+    border-top: 1px solid #ccc;
+  }
+`;
