36fa630bb9ce4261ebd2938f53364851ca60a95e
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-09-05 18:56:48 +0200

    update export

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-// import '../styles/globals.css';
-
 import AppLayout from '../components/AppLayout/AppLayout';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <AppLayout>
       <Component {...pageProps} />
     </AppLayout>
   );
 }
-
-export default MyApp;
