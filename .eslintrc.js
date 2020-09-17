fb85379be0e3bee6110d6392ad9d6f43f12352f3
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-09-17 22:14:45 +0200

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
     'no-restricted-syntax': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
+    'consistent-return': 'off',
   },
 };
