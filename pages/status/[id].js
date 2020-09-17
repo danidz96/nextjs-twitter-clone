ccb3a9b4bcaa78d0d083bcb4ebd88d30da4523a2
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-09-17 22:44:40 +0200

    render a Devit with data

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,5 +1,21 @@
-import React from 'react';
+import Devit from '../../components/Devit';
 
-export default function DevitPage() {
-  return <div>devit page</div>;
+export default function DevitPage(props) {
+  return (
+    <>
+      <Devit {...props} />
+    </>
+  );
 }
+
+DevitPage.getInitialProps = (context) => {
+  const { query } = context;
+  const { id } = query;
+
+  return fetch(`http://localhost:3000/api/devits/${id}`).then((apiResponse) => {
+    if (apiResponse.ok) return apiResponse.json();
+    if (apiResponse) {
+      apiResponse.writeHead(301, { Location: '/home' }).end();
+    }
+  });
+};
